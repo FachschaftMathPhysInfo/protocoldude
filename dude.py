81f4a855a67d40bff5fefefead6e4642dcbe3141
Lennart Stipulkowski <stipulkowski@stud.uni-heidelberg.de>
2019-03-01 19:53:46 +0100

    Format code according to black https://github.com/ambv/black

--- dude.py
+++ dude.py
@@ -56,22 +56,25 @@ def check_path(path: str) -> bool:
     year = path[0:4].isnumeric()
     month = path[5:7].isnumeric()
     date = path[8:9].isnumeric()
-    name = year and month and date and path[4] is '-' and path[7] is '-'
+    name = year and month and date and path[4] is "-" and path[7] is "-"
 
-    if not path.endswith('.txt'):
-        raise Exception('Der Dateipfad führt nicht zu einem Sitzungsprotokoll oder du schaust besser nochmal über den Filenamen!')
+    if not path.endswith(".txt"):
+        raise Exception(
+            "Der Dateipfad führt nicht zu einem Sitzungsprotokoll oder du schaust besser nochmal über den Filenamen!"
+        )
     return True
 
 
 class Protocol(object):
     """reads in the protocol and processes it"""
+
     def __init__(self, path):
         # validate filename as protocol (yyyy-mm-dd) and .txt
         self.path = path
 
         print('\nProtokoll "{}" wird bearbeitet .. \n \n'.format(self.path))
 
-        with open(self.path, 'r') as file:
+        with open(self.path, "r") as file:
             self.protocol = file.read().splitlines()
         self.tops = []
         self.mails = False
@@ -91,20 +94,27 @@ class Protocol(object):
 
         for i in range(len(title_lines) - 1):
             begin = title_lines[i] + 2
-            end = title_lines[i+1] - 1
+            end = title_lines[i + 1] - 1
             top = TOP(i + 1, begin, end)
             self.tops.append(top)
 
     def rename_title(self):
         """Adjust TOP title type setting"""
         for top in self.tops:
-            if not self.protocol[top.start+1].startswith("TOP: "):
-                self.protocol[top.start+1] = "TOP " + str(top.number) + ": " + self.protocol[top.start+1]
+            if not self.protocol[top.start + 1].startswith("TOP: "):
+                self.protocol[top.start + 1] = (
+                    "TOP " + str(top.number) + ": " + self.protocol[top.start + 1]
+                )
             else:
-                self.protocol[top.start+1] = self.protocol[top.start+1][:3] + str(top.number) + " " + self.protocol[top.start+1][3:]
-            length = len(self.protocol[top.start+1])
-            self.protocol[top.start+2] = "="*length
-            self.protocol[top.start] = "="*length
+                self.protocol[top.start + 1] = (
+                    self.protocol[top.start + 1][:3]
+                    + str(top.number)
+                    + " "
+                    + self.protocol[top.start + 1][3:]
+                )
+            length = len(self.protocol[top.start + 1])
+            self.protocol[top.start + 2] = "=" * length
+            self.protocol[top.start] = "=" * length
 
     def get_users(self):
         for top in self.tops:
@@ -114,8 +124,10 @@ class Protocol(object):
     def send_mails(self):
         try:
             server = smtplib.SMTP("mail.urz.uni-heidelberg.de", 587)
-            login = input('Uni ID für den Mailversand: ')
-            server.login(login, getpass.getpass(prompt='Passwort für deinen Uni Account: '))
+            login = input("Uni ID für den Mailversand: ")
+            server.login(
+                login, getpass.getpass(prompt="Passwort für deinen Uni Account: ")
+            )
 
             for top in self.tops:
                 top.send_mail(server, self.protocol)
@@ -124,24 +136,43 @@ class Protocol(object):
             print("\nAlle Mails wurden erfolgreich verschickt. \n")
         except Exception as e:
             print(e.what())
-            print("\nMails konnten nicht verschickt werden. Hast du die richtigen Anmeldedaten eingegeben?")
+            print(
+                "\nMails konnten nicht verschickt werden. Hast du die richtigen Anmeldedaten eingegeben?"
+            )
 
     def write_success(self):
         if self.mails:
             now = datetime.datetime.now()
-            self.protocol.insert(0, ":Protocoldude: Mails versandt @ {}".format(now.strftime("%H:%M %d.%m.%Y")))
+            self.protocol.insert(
+                0,
+                ":Protocoldude: Mails versandt @ {}".format(
+                    now.strftime("%H:%M %d.%m.%Y")
+                ),
+            )
             self.protocol.insert(1, "\n")
 
-        with open(self.path, 'w') as file:
-            file.write('\n'.join(self.protocol) + '\n')
+        with open(self.path, "w") as file:
+            file.write("\n".join(self.protocol) + "\n")
         # TODO: more specific exception handling
         try:
-            subprocess.run(['svn', 'up'], check=True)
-            subprocess.run(['svn', 'add', '{}'.format(self.path)], check=True)
-            subprocess.run(['svn', 'commit', '-m', '"Protokoll der gemeinsamen Sitzung hinzugefügt"'], check=True)
-            print("Protokoll bearbeitet und in den Sumpf geschrieben.\n Für heute hast du's geschafft!")
+            subprocess.run(["svn", "up"], check=True)
+            subprocess.run(["svn", "add", "{}".format(self.path)], check=True)
+            subprocess.run(
+                [
+                    "svn",
+                    "commit",
+                    "-m",
+                    '"Protokoll der gemeinsamen Sitzung hinzugefügt"',
+                ],
+                check=True,
+            )
+            print(
+                "Protokoll bearbeitet und in den Sumpf geschrieben.\n Für heute hast du's geschafft!"
+            )
         except:
-            print("Konnte SVN Update nicht durchführen. \n Das musst Du irgendwie von Hand reparieren mit 'svn cleanup' oder so.")
+            print(
+                "Konnte SVN Update nicht durchführen. \n Das musst Du irgendwie von Hand reparieren mit 'svn cleanup' oder so."
+            )
             print("Das Protokoll wurde trotzdem bearbeitet und gespeichert.")
 
 
@@ -161,9 +192,9 @@ class TOP(Protocol):
     def get_user(self, protocol: list):
         """searches for all mentioned users in the TOP paragraph"""
         users = []
-        for line in protocol[self.start:self.end]:
+        for line in protocol[self.start : self.end]:
             # check for mail address
-            adress = re.findall('\$\{(.*?)\}', line)
+            adress = re.findall("\$\{(.*?)\}", line)
             users += adress
         self.users = list(set(users))  # remove duplicates
 
@@ -180,18 +211,20 @@ class TOP(Protocol):
             fromaddr = "fachschaft@mathphys.stura.uni-heidelberg.de"
 
             msg = MIMEMultipart()
-            msg['From'] = fromaddr
-            msg['To'] = mail
-            msg['Subject'] = "Gemeinsame Sitzung: {}".format(protocol[self.start+1])
+            msg["From"] = fromaddr
+            msg["To"] = mail
+            msg["Subject"] = "Gemeinsame Sitzung: {}".format(protocol[self.start + 1])
 
             if user in LIST_USERS[:][0]:
                 body = LIST_USERS[LIST_USERS[:][0].index(user)][1] + ",\n\n"
             else:
                 body = "Hallo {},\n\n".format(user)
-            body += "Du sollst über irgendwas informiert werden. Im Sitzungsprotokoll steht dazu folgendes:\n\n{}\n\n\nViele Grüße, Dein SPAM-Skript.".format('\n'.join(protocol[self.start:self.end])+'\n')
+            body += "Du sollst über irgendwas informiert werden. Im Sitzungsprotokoll steht dazu folgendes:\n\n{}\n\n\nViele Grüße, Dein SPAM-Skript.".format(
+                "\n".join(protocol[self.start : self.end]) + "\n"
+            )
             # \n\nSollte der Text abgeschnitten sein, schaue bitte im Sitzungsprotokoll nach (Zeile #{tops[i]} – MathPhys Login notwendig).\n#{url}/#{file}\" | mail -a \"Reply-To: #{$replyto}\" -a \"Content-Type: text/plain; charset=UTF-8\" -s \"#{$subject}: #{title} (#{date})\" '#{mail}';", false) unless $debug
 
-            msg.attach(MIMEText(body, 'plain'))
+            msg.attach(MIMEText(body, "plain"))
 
             text = msg.as_string()
             server.sendmail(fromaddr, mail, text)
@@ -199,14 +232,10 @@ class TOP(Protocol):
 
 def ldap_search(users: list) -> list:
     """ searches for a list of users in our ldap """
-    server = ldap.initialize('ldaps://' + MATHPHYS_LDAP_ADDRESS)
-    users = ['(uid={})'.format(user) for user in users]
-    query = '(|{})'.format("".join(users))
-    query_result = server.search_s(
-        MATHPHYS_LDAP_BASE_DN,
-        ldap.SCOPE_SUBTREE,
-        query
-    )
+    server = ldap.initialize("ldaps://" + MATHPHYS_LDAP_ADDRESS)
+    users = ["(uid={})".format(user) for user in users]
+    query = "(|{})".format("".join(users))
+    query_result = server.search_s(MATHPHYS_LDAP_BASE_DN, ldap.SCOPE_SUBTREE, query)
     return query_result
 
 
@@ -217,8 +246,9 @@ def extract_mails(query: list) -> list:
         for result in query:
             # dn = result[0]
             attributes = result[1]
-            mails.append(attributes["mail"][0].decode('utf-8'))
+            mails.append(attributes["mail"][0].decode("utf-8"))
     return mails
+
 
 def main():
     # disables error messages
@@ -228,15 +258,15 @@ def main():
     parser.add_argument(
         "infile",
         metavar="./path/to/file",
-        type=argparse.FileType('r'),
-        help="Path to the protcol. Expects filename to have have the following format: 'yyyy-mm-dd.txt'"
+        type=argparse.FileType("r"),
+        help="Path to the protcol. Expects filename to have have the following format: 'yyyy-mm-dd.txt'",
     )
     parser.add_argument(
         "-v",
         "--version",
         help="Print the version and exit.",
         action="version",
-        version=__version__
+        version=__version__,
     )
 
     args = parser.parse_args()
