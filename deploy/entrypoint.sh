54ed01e5f3de7fb994a78583155186dfaabf905d
Christoph Blattgerste <chris-blattgerste@gmx.de>
2021-07-22 00:18:11 +0200

    added svn add logic

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $INPUT_MY_NAME"
-sh -C "uname -a"
+# sh -c "echo Hello world my name is $INPUT_MY_NAME"
+ssh -v -i __TEMP_INPUT_KEY_FILE -P "${INPUT_PORT}" "${INPUT_USER}"@"${INPUT_HOST}" 
+cd sumpf
+svn update
+svn add protocoldude3.py
+svn commit -m"Protocoldude3: auto new version"
